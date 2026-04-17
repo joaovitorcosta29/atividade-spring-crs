3ef4648b1cf1f27412d1205c97ba4a199be85475
joaovitorcosta29 <joaovitorcostalima81@gmail.com>
2026-04-17 16:34:59 -0300

    inicializacao-do-projeto

---
+++ src/main/java/com/atividade/atividadecrs/AtividadecrsApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.atividadecrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadecrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadecrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividade/atividadecrs/AtividadecrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.atividadecrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadecrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
